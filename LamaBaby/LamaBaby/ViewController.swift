56aa863b733e419c69ad5e662262897404aa34ab
Luke Phyall <shoreditchwarlock@gmail.com>
2019-04-17 11:37:03 +0100

    Initializes project.

---
+++ LamaBaby/LamaBaby/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LamaBaby
+//
+//  Created by Luke Phyall on 17/04/2019.
+//  Copyright © 2019 Sexy Lama Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
